247401ded3d0d3764ee55f87df3a00e48be0ce95
redfang110 <r3dfang110@gmail.com>
2024-05-09 17:17:07 -0500

    almost finished

--- src/frontend/src/Account.js
+++ src/frontend/src/Account.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SubmitRecipe from './SubmitRecipe';
 import axios from 'axios';
 
@@ -8,19 +8,50 @@ function Account({ userId }) {
     const [oldPassword, setOldPassword] = useState('');
     const [newPassword, setNewPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+    const [usersRecipes, setUsersRecipes] = useState([]);
 
     const handleSubmitEdit = async () => {
         if (newPassword === confirmPassword) {
             const updateUser = {
+                userId: userId,
                 oldPassword: oldPassword,
                 newPassword: newPassword
             }
+            try {
             const response = await axios.post(
                 "http://localhost:4000/api/users/change-password",
                 updateUser
               );
+            } catch (error) {
+              console.error('Failed to update password:', error);
+          }
         }
     }
+
+    useEffect(() => {
+      async function fetchRecipes() {
+          try {
+              const response = await axios.get('http://localhost:4000/api/recipes');
+              const user = await axios.get(`http://localhost:4000/api/users/${userId}`);
+              let recipes = [];
+              console.log("fetch");
+              console.log(user);
+              console.log(user.userRecipes);
+              response.data.forEach(function(recipe) {
+                console.log(recipe.name);
+                if (user.data.userRecipes && user.data.userRecipes.includes(recipe.name)) {
+                  recipes.push(recipe);
+                }
+              });
+              setUsersRecipes(recipes);
+              console.log(recipes);
+          } catch (error) {
+              console.error('Failed to fetch recipes:', error);
+          }
+      }
+
+      fetchRecipes();
+  }, [userId]);
 
     let jsxComponent;
     switch(view) {
@@ -134,10 +165,48 @@ function Account({ userId }) {
         </div>
             break;
         case "myRecipes":
+          jsxComponent = <div>
+            <div style={{ textAlign: 'center' }}>
+              <h2 className="mb-4">Recipes</h2>
+            </div>
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                {usersRecipes.length > 0 ? (
+                    usersRecipes.map(recipe => (
+                        <div key={recipe._id} className="col">
+                            <div className="card">
+                                <div className="image-container" style={{ maxHeight: 400, maxWidth: 350, textAlign: "center" }}>
+                                    <img src={recipe.imageUrl} className="card-img-top" alt={recipe.name} style={{ objectFit: "contain", maxHeight: 250, maxWidth: 250, height: "auto", width: "auto" }}/>
+                                </div>
+                                <div className="card-body">
+                                    <h5 className="card-title">{recipe.name}</h5>
+                                    <p className="card-text">{recipe.description}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                ) : (
+                    <p>No recipes found.</p>
+                )}
+            </div>
+            <div style={{ textAlign: 'center', position: 'absolute', left: "45%", bottom: "10%" }}>
+            <button
+                onClick={() => setView("home")}
+                style={{
+                  width: "200px",
+                  fontWeight: "bold",
+                  borderRadius: 20,
+                  backgroundColor: "blue",
+                  color: "white",
+                  marginTop: "10px"
+                }}
+              >Back</button>
+            </div>
+          </div>
             break;
         case "submitRecipes":
             jsxComponent = <div>
                 <SubmitRecipe userId={userId}/>
+                <div style={{ textAlign: 'center', position: 'absolute', left: "45%", bottom: "10%" }}>
                 <button
                 onClick={() => setView("home")}
             style={{
@@ -150,6 +219,7 @@ function Account({ userId }) {
           >
             Back
           </button>
+          </div>
                 </div>
             break;
     }
